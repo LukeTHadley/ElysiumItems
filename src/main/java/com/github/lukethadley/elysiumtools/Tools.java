525b2683777d6e414098326f42cdeb198db4d360
LukeTHadley <lukehadley2001@gmail.com>
2021-04-09 11:39:11 +0100

    Created CustomToolInterface to be used later on during Enum>Object refractor.

--- src/main/java/com/github/lukethadley/elysiumtools/Tools.java
+++ src/main/java/com/github/lukethadley/elysiumtools/Tools.java
@@ -10,33 +10,19 @@ import java.util.Map;
 public enum Tools {
 
     //BOWS
-    ENDER_BOW(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Ender Bow",
-            "Shoot an Ender Pearl with a bow and go where it lands",
-            new String[]{ChatColor.GRAY + "Shoot Pearls! Not Arrows!", ChatColor.GRAY + "You must have an " + ChatColor.LIGHT_PURPLE + "Ender Pearl", ChatColor.GRAY +  "In your inventory to use this bow!"},
-            Material.BOW,
-            ImmutableMap.of(
-                    Enchantment.ARROW_INFINITE, 1),
-            false
-            ), //The bow needs to have infinity otherwise it takes an arrow out of the players inventory even when cancelled.
-    SHOTGUN_BOW(ChatColor.BLUE + "" + ChatColor.BOLD + "Shotgun Bow",
-            "Shoots a volly of 5 arrows",
-            new String[]{ChatColor.GRAY + "Shoot a volly of 5 arrows at once!", ChatColor.GRAY + "You must have at least", ChatColor.GRAY + "5 arrows in your inventory."},
-            Material.BOW,
-            ImmutableMap.of(
-                    Enchantment.ARROW_INFINITE, 1),
-            false
-            ),
+    ENDER_BOW(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Ender Bow", "Shoot an Ender Pearl with a bow and go where it lands", new String[]{ChatColor.GRAY + "Shoot Pearls! Not Arrows!", ChatColor.GRAY + "You must have an " + ChatColor.LIGHT_PURPLE + "Ender Pearl", ChatColor.GRAY +  "In your inventory to use this bow!"}, Material.BOW, ImmutableMap.of(Enchantment.ARROW_INFINITE, 1), false), //The bow needs to have infinity otherwise it takes an arrow out of the players inventory even when cancelled.
+    SHOTGUN_BOW(ChatColor.BLUE + "" + ChatColor.BOLD + "Shotgun Bow", "Shoots a volly of 5 arrows", new String[]{ChatColor.GRAY + "Shoot a volly of 5 arrows at once!", ChatColor.GRAY + "You must have at least", ChatColor.GRAY + "5 arrows in your inventory."}, Material.BOW, ImmutableMap.of(Enchantment.ARROW_INFINITE, 1), false),
 
 
     //Swords
-    DEMONIC_SWORD(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Demonic Sword",
-            "Has a 20% chance of applying wither effect to your enemy for 10 seconds",
-            new String[]{ChatColor.DARK_PURPLE + "Forged in the depths of hell", ChatColor.GRAY + "Has a 20% chance of applying the", ChatColor.GRAY + "wither effect to your enemy for 10 seconds"},
-            Material.STONE_SWORD,
-            ImmutableMap.of(
-                    Enchantment.DURABILITY, 6),
-            true
-            );
+    DEMONIC_SWORD(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Demonic Sword", "Has a 20% chance of applying wither effect to your enemy for 10 seconds", new String[]{ChatColor.DARK_PURPLE + "Forged in the depths of hell", ChatColor.GRAY + "Has a 20% chance of applying the", ChatColor.GRAY + "wither effect to your enemy for 10 seconds"}, Material.STONE_SWORD, ImmutableMap.of(Enchantment.DURABILITY, 6), true);
+
+    //Volatile Tools
+    //VOLATILE_SWORD("&6&lVolatile Sword", "This item will expload when it breaks!", new String[]{"&7This item will expload when it breaks!"}, Material.GOLDEN_SWORD, ),
+    //VOLATILE_AXE(),
+    //VOLATILE_SHOVEL(),
+    //VOLATILE_PICKAXE(),
+    //VOLATILE_HOE();
 
 
     String name, description;
