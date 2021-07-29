96a612ecae46cf38d5da7bdfe3fd8b7f7bb02228
LukeTHadley <lukehadley2001@gmail.com>
2021-07-29 12:34:48 +0100

    Fixed NullPointerException error thrown when other bows are shot.
    Added a Try/Catch clause to the bow listeners to avoid an error when a bow is shot without metadata.

--- src/main/java/com/github/lukethadley/elysiumitems/listeners/tools/EnderBowListener.java
+++ src/main/java/com/github/lukethadley/elysiumitems/listeners/tools/EnderBowListener.java
@@ -16,6 +16,7 @@ package com.github.lukethadley.elysiumitems.listeners.tools;
 
 import com.github.lukethadley.elysiumitems.ToolsMessages;
 import com.github.lukethadley.elysiumitems.items.tools.bows.EnderBow;
+import org.apache.commons.lang.ObjectUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -40,40 +41,43 @@ public class EnderBowListener implements Listener {
 
     @EventHandler
     public void enderBowEvent(EntityShootBowEvent e){
-
-        if (e.getEntity() instanceof Player){ //Make sure the entity shooting this is a player
-            Player player = (Player) e.getEntity();
-            List<String> enderBowLore = Arrays.asList(enderBow.getLore());
-            if (enderBowLore == null){
-                return;
-            }
-
-            if (e.getBow().getItemMeta().getLore().equals(enderBowLore)){ // Ensure that it is the enderbow
-                if(player.getGameMode() != GameMode.CREATIVE){ //If the player is in creative then we can let them fire the bow regardless of whether they have ender pearls
-                    if(!player.getInventory().contains(Material.ENDER_PEARL)){ //Check if the player's inventory contains any ender pearls
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.ENDER_BOW_NO_ENDERPEARL_EVENT)); //Send them a message
-                        e.setConsumeItem(false);
-                        e.setCancelled(true); //Cancel the event so an arrow isn't shot
-                        return;
-                    }
-                    else{
-                        ItemStack ender = new ItemStack(Material.ENDER_PEARL, 1); //Create an ItemStack of 1 ender pearl
-                        player.getInventory().removeItem(ender); //Use the created ItemStack to remove 1 ender pearl from the player's inventory
-                    }
+        try{
+            if (e.getEntity() instanceof Player){ //Make sure the entity shooting this is a player
+                Player player = (Player) e.getEntity();
+                List<String> enderBowLore = Arrays.asList(enderBow.getLore());
+                if (enderBowLore == null){
+                    return;
                 }
-                //We want to get the arrow entity so we can use it's velocity for the ender pearl then remove it
-                Entity arrow = e.getProjectile();
 
-                //Since an EnderPearl is a projectile we can create it easily by specifying it's class object and providing the velocity we want to shoot it at
-                EnderPearl pearl = player.launchProjectile(EnderPearl.class, arrow.getVelocity());
+                if (e.getBow().getItemMeta().getLore().equals(enderBowLore)){ // Ensure that it is the enderbow
+                    if(player.getGameMode() != GameMode.CREATIVE){ //If the player is in creative then we can let them fire the bow regardless of whether they have ender pearls
+                        if(!player.getInventory().contains(Material.ENDER_PEARL)){ //Check if the player's inventory contains any ender pearls
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.ENDER_BOW_NO_ENDERPEARL_EVENT)); //Send them a message
+                            e.setConsumeItem(false);
+                            e.setCancelled(true); //Cancel the event so an arrow isn't shot
+                            return;
+                        }
+                        else{
+                            ItemStack ender = new ItemStack(Material.ENDER_PEARL, 1); //Create an ItemStack of 1 ender pearl
+                            player.getInventory().removeItem(ender); //Use the created ItemStack to remove 1 ender pearl from the player's inventory
+                        }
+                    }
+                    //We want to get the arrow entity so we can use it's velocity for the ender pearl then remove it
+                    Entity arrow = e.getProjectile();
 
-                //Now that we have spawned the ender pearl we want to remove the arrow
-                arrow.remove();
+                    //Since an EnderPearl is a projectile we can create it easily by specifying it's class object and providing the velocity we want to shoot it at
+                    EnderPearl pearl = player.launchProjectile(EnderPearl.class, arrow.getVelocity());
 
-                //The final step is the set the shooter to the player so it actually teleports them when it hits the ground
-                pearl.setShooter(player);
+                    //Now that we have spawned the ender pearl we want to remove the arrow
+                    arrow.remove();
 
+                    //The final step is the set the shooter to the player so it actually teleports them when it hits the ground
+                    pearl.setShooter(player);
+
+                }
             }
+        } catch (NullPointerException exception){
+            return;
         }
     }
 
--- src/main/java/com/github/lukethadley/elysiumitems/listeners/tools/ShotgunBowListener.java
+++ src/main/java/com/github/lukethadley/elysiumitems/listeners/tools/ShotgunBowListener.java
@@ -39,72 +39,75 @@ public class ShotgunBowListener implements Listener {
 
     @EventHandler
     public void shotgunBowEvent(EntityShootBowEvent e){
-
-        if (e.getEntity() instanceof Player){
-            Player player = (Player) e.getEntity();
-            List<String> shotgunBowLore = Arrays.asList(shotgunBow.getLore());
-            if (shotgunBowLore == null){
-                return;
-            }
-
-            if (e.getBow().getItemMeta().getLore().equals(shotgunBowLore)){// Ensure that it is the shotgun bow
-
-
-                if (!onCooldown(player)){
-                    if(player.getGameMode() != GameMode.CREATIVE){ //If the player is in creative then we can let them fire the bow regardless
-
-                        if(!player.getInventory().contains(Material.ARROW, 5)){ //Check if the player's inventory doesn't contain 5 arrows
-                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.SHOTGUN_BOW_NOT_ENOUGH_ARROWS)); //Send them a message
-                            e.setConsumeItem(false);
-                            e.setCancelled(true); //Cancel the event so an arrow isn't shot
-                            return;
-                        }
-                        else{
-                            ItemStack arrows = new ItemStack(Material.ARROW, 5); //Create an ItemStack of 1 ender pearl
-                            player.getInventory().removeItem(arrows); //Use the created ItemStack to remove 1 ender pearl from the player's inventory
-                        }
-                    }
-
-
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',ToolsMessages.SHOTGUN_BOW_USE_MESSAGE));
-
-
-                    Entity arrow = e.getProjectile();
-                    Vector velocity = arrow.getVelocity();
-
-
-                    Vector v1 = new Vector(velocity.getX(), velocity.getY(), velocity.getZ());
-
-                    Vector v2 = new Vector(velocity.getX(), velocity.getY() - (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getZ() - (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND);
-                    Vector v3 = new Vector(velocity.getX(), velocity.getY() + (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getZ() + (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND);
-                    Vector v4 = new Vector(velocity.getX() - (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getY(), velocity.getZ());
-                    Vector v5 = new Vector(velocity.getX() + (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getY(), velocity.getZ());
-
-                    Arrow arrow1 = player.launchProjectile(Arrow.class, v1);
-                    arrow1.setShooter(player);
-
-                    Arrow arrow2 = player.launchProjectile(Arrow.class, v2);
-                    arrow2.setShooter(player);
-
-                    Arrow arrow3 = player.launchProjectile(Arrow.class, v3);
-                    arrow3.setShooter(player);
-
-                    Arrow arrow4 = player.launchProjectile(Arrow.class, v4);
-                    arrow4.setShooter(player);
-
-                    Arrow arrow5 = player.launchProjectile(Arrow.class, v5);
-                    arrow5.setShooter(player);
-
-
-                    arrow.remove();
+        try{
+            if (e.getEntity() instanceof Player){
+                Player player = (Player) e.getEntity();
+                List<String> shotgunBowLore = Arrays.asList(shotgunBow.getLore());
+                if (shotgunBowLore == null){
                     return;
+                }
 
+                if (e.getBow().getItemMeta().getLore().equals(shotgunBowLore)){// Ensure that it is the shotgun bow
+
+
+                    if (!onCooldown(player)){
+                        if(player.getGameMode() != GameMode.CREATIVE){ //If the player is in creative then we can let them fire the bow regardless
+
+                            if(!player.getInventory().contains(Material.ARROW, 5)){ //Check if the player's inventory doesn't contain 5 arrows
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.SHOTGUN_BOW_NOT_ENOUGH_ARROWS)); //Send them a message
+                                e.setConsumeItem(false);
+                                e.setCancelled(true); //Cancel the event so an arrow isn't shot
+                                return;
+                            }
+                            else{
+                                ItemStack arrows = new ItemStack(Material.ARROW, 5); //Create an ItemStack of 1 ender pearl
+                                player.getInventory().removeItem(arrows); //Use the created ItemStack to remove 1 ender pearl from the player's inventory
+                            }
+                        }
+
+
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',ToolsMessages.SHOTGUN_BOW_USE_MESSAGE));
+
+
+                        Entity arrow = e.getProjectile();
+                        Vector velocity = arrow.getVelocity();
+
+
+                        Vector v1 = new Vector(velocity.getX(), velocity.getY(), velocity.getZ());
+
+                        Vector v2 = new Vector(velocity.getX(), velocity.getY() - (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getZ() - (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND);
+                        Vector v3 = new Vector(velocity.getX(), velocity.getY() + (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getZ() + (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND);
+                        Vector v4 = new Vector(velocity.getX() - (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getY(), velocity.getZ());
+                        Vector v5 = new Vector(velocity.getX() + (Math.random() * (LOWER_BOUND - UPPER_BOUND)) + UPPER_BOUND, velocity.getY(), velocity.getZ());
+
+                        Arrow arrow1 = player.launchProjectile(Arrow.class, v1);
+                        arrow1.setShooter(player);
+
+                        Arrow arrow2 = player.launchProjectile(Arrow.class, v2);
+                        arrow2.setShooter(player);
+
+                        Arrow arrow3 = player.launchProjectile(Arrow.class, v3);
+                        arrow3.setShooter(player);
+
+                        Arrow arrow4 = player.launchProjectile(Arrow.class, v4);
+                        arrow4.setShooter(player);
+
+                        Arrow arrow5 = player.launchProjectile(Arrow.class, v5);
+                        arrow5.setShooter(player);
+
+
+                        arrow.remove();
+                        return;
+
+
+                    }
+                    e.setCancelled(true);
 
                 }
-                e.setCancelled(true);
-
+                return;
             }
-            return;
+        } catch (NullPointerException exception){
+
         }
     }
 
