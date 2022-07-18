0b0c11c4c448b27a11589526d2a440bc580422fd
LukeTHadley <lukehadley2001@gmail.com>
2022-07-18 17:29:04 +0100

    Changed Scoober set name/lores

--- src/main/java/com/github/lukethadley/elysiumitems/items/armor/helmets/ScooberHelmet.java
+++ src/main/java/com/github/lukethadley/elysiumitems/items/armor/helmets/ScooberHelmet.java
@@ -14,9 +14,9 @@ import java.util.Arrays;
 public class ScooberHelmet extends CustomItem {
 
     private static final String NAME = "ScooberHelmet";
-    private static final String ITEM_DISPLAY_NAME = "&1&lScoober Helmet";
+    private static final String ITEM_DISPLAY_NAME = net.md_5.bungee.api.ChatColor.of("#4d94ff") + "&lScoober Helmet";
     private static final String DESCRIPTION = "When worn in the full set, the user will gain night vision/water breathing when under water!";
-    private static final String[] ITEM_LORE = new String[]{"&7When worn in the full set, ", "&7the user will gain night", "&7vision/water breathing when under water!"};
+    private static final String[] ITEM_LORE = new String[]{net.md_5.bungee.api.ChatColor.of("#4d94ff") + "Deep Sea Diving!", "&7When worn in the full set the", "&7user will gain night vision/water", "&7breathing when under water!"};
 
     public ScooberHelmet(){
         super(NAME, ITEM_DISPLAY_NAME, DESCRIPTION, ITEM_LORE, Material.LEATHER_HELMET);
