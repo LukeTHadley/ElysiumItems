b8033a31ce4355b527b5b80439030d35b05b9453
LukeTHadley <lukehadley2001@gmail.com>
2022-07-19 21:38:21 +0100

    Fixed issue with mcMMO taking priority on PlayerFishEvent

--- src/main/java/com/github/lukethadley/elysiumitems/listeners/tools/CodFatherListener.java
+++ src/main/java/com/github/lukethadley/elysiumitems/listeners/tools/CodFatherListener.java
@@ -4,31 +4,32 @@ import de.tr7zw.changeme.nbtapi.NBTItem;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class CodFatherListener implements Listener {
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGHEST)
     public void PlayerFishEvent(PlayerFishEvent event) {
-
         if (event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
-            ItemStack fishingRod = event.getPlayer().getItemInHand();
+            ItemStack fishingRod = event.getPlayer().getInventory().getItemInMainHand();
             if (fishingRod.getType() != Material.FISHING_ROD){
                 return;
             }
             NBTItem nbti = new NBTItem(fishingRod);
-
             String plugin = nbti.getString("plugin");
             String item = nbti.getString("item");
             if (plugin.equals("Elysium-Items")) {
                 if (item.equals("CodFatherRod")) {
                     Item stack = (Item) event.getCaught();
+                    event.getPlayer().sendRawMessage(stack.getItemStack().getType().name());
+
                     if (stack.getItemStack().getType() == Material.COD){
                         stack.setItemStack(new ItemStack(Material.COOKED_COD)); //Your new itemstack here!
                     }
-                    if (stack.getItemStack().getType() == Material.SALMON) {
+                    else if (stack.getItemStack().getType() == Material.SALMON) {
                         stack.setItemStack(new ItemStack(Material.COOKED_SALMON)); //Your new itemstack here!
                     }
 
