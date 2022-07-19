23a9c707b727a7d0590cba403a93acbe5419bec5
LukeTHadley <lukehadley2001@gmail.com>
2022-07-19 20:12:29 +0100

    Set up command permissions

--- src/main/java/com/github/lukethadley/elysiumitems/ToolsCommandHandler.java
+++ src/main/java/com/github/lukethadley/elysiumitems/ToolsCommandHandler.java
@@ -31,56 +31,61 @@ public class ToolsCommandHandler implements TabExecutor {
         }
         if (args.length >= 1){ //Some arguments given
             if (command.getName().equals("elysiumtools")){ //It was the elysiumtools command
-                if (args[0].equalsIgnoreCase("list")){ //It was the list command, list the items
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.LIST_ALL_TOOLS_HEADER));
-                    for (CustomItem item : items.getCustomItems().values()) {
-                        sender.sendMessage(item.getName() + ChatColor.GRAY + " - " + item.getDescription());
-                    }
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.LIST_ALL_TOOLS_FOOTER));
-                    return true;
-                }
-                if (args[0].equalsIgnoreCase("give")) { //It was the give command
-                    if (args.length >= 2){ //If there was an item given as an argument
-                        if (items.checkExist(args[1])){ //Check that the given string corresponds to an item
-                            CustomItem item = items.getCustomItems().get(args[1]); //Get the CustomItem object
-                            if (args.length >= 3){ //If a specific player was specified
-                                Player playerToGiveTo = plugin.getServer().getPlayer(args[2]);
-                                if (playerToGiveTo != null){ //And the player exists
-                                    playerToGiveTo.getInventory().addItem(item.getItem()); //Give to the player
-                                    sender.sendMessage("You gave " + item.getName() + " to " + playerToGiveTo.getName());
-                                    playerToGiveTo.sendMessage("You were given " + item.getName());
-                                    plugin.getServer().getLogger().info(playerToGiveTo.getName() + " was given " + item.getName() + " by " + sender.getName());
-                                    return true;
 
+                if (sender.hasPermission("elysiumitems.givetools")){
 
-                                }
-                                else { //If the player did not exist
-                                    sender.sendMessage("That player does not exist!");
-                                }
-                            }
-                            else {
-                                if (sender instanceof Player){ //If a player wasn't specified, and the sender was a player, give it to that player
-                                    Player p = (Player) sender;
-                                    p.getInventory().addItem(item.getItem());
-                                    sender.sendMessage("You were given " + item.getName());
-                                    plugin.getServer().getLogger().info(sender.getName() + " was given " + item.getName() + " by themselves");
-                                    return true;
-                                }
-                                else { //Console should not be given items, tell them off!
-                                    sender.sendMessage("You must specify a player as console!");
-                                }
-                            }
+                    if (args[0].equalsIgnoreCase("list")){ //It was the list command, list the items
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.LIST_ALL_TOOLS_HEADER));
+                        for (CustomItem item : items.getCustomItems().values()) {
+                            sender.sendMessage(item.getName() + ChatColor.GRAY + " - " + item.getDescription());
                         }
-                        else { //If the item doesn't exist
-                            sender.sendMessage("That custom item does not exist");
-                        }
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ToolsMessages.LIST_ALL_TOOLS_FOOTER));
                         return true;
                     }
-                    else { //If the item was not specified
-                        sender.sendMessage("Please specify a custom item");
-                    }
+                    if (args[0].equalsIgnoreCase("give")) { //It was the give command
+                        if (args.length >= 2){ //If there was an item given as an argument
+                            if (items.checkExist(args[1])){ //Check that the given string corresponds to an item
+                                CustomItem item = items.getCustomItems().get(args[1]); //Get the CustomItem object
+                                if (args.length >= 3){ //If a specific player was specified
+                                    Player playerToGiveTo = plugin.getServer().getPlayer(args[2]);
+                                    if (playerToGiveTo != null){ //And the player exists
+                                        playerToGiveTo.getInventory().addItem(item.getItem()); //Give to the player
+                                        sender.sendMessage("You gave " + item.getName() + " to " + playerToGiveTo.getName());
+                                        playerToGiveTo.sendMessage("You were given " + item.getName());
+                                        plugin.getServer().getLogger().info(playerToGiveTo.getName() + " was given " + item.getName() + " by " + sender.getName());
+                                        return true;
 
+
+                                    }
+                                    else { //If the player did not exist
+                                        sender.sendMessage("That player does not exist!");
+                                    }
+                                }
+                                else {
+                                    if (sender instanceof Player){ //If a player wasn't specified, and the sender was a player, give it to that player
+                                        Player p = (Player) sender;
+                                        p.getInventory().addItem(item.getItem());
+                                        sender.sendMessage("You were given " + item.getName());
+                                        plugin.getServer().getLogger().info(sender.getName() + " was given " + item.getName() + " by themselves");
+                                        return true;
+                                    }
+                                    else { //Console should not be given items, tell them off!
+                                        sender.sendMessage("You must specify a player as console!");
+                                    }
+                                }
+                            }
+                            else { //If the item doesn't exist
+                                sender.sendMessage("That custom item does not exist");
+                            }
+                            return true;
+                        }
+                        else { //If the item was not specified
+                            sender.sendMessage("Please specify a custom item");
+                        }
+
+                    }
                 }
+
             }
             return true;
         }
